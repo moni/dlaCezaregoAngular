b73fb3e883c0842c77bcd1898bd13b605f3a11f8
moni <monika.rusajczyk@gmail.com>
2019-03-26 16:35:48 +0100

    #177

--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+import {LazyLoadImageModule} from "ng-lazyload-image";
+
 import { DisplayNewsComponent } from './components/display-news/display-news.component';
 import { AllNewsComponent } from './components/all-news/all-news.component';
 import { TodayNewsComponent } from './components/today-news/today-news.component';
-import {RouterModule} from '@angular/router';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
-import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
 import { SourcesMultiSelectDropdownComponent } from './components/sources-multi-select-dropdown/sources-multi-select-dropdown.component';
+
+
 
 @NgModule({
 
@@ -16,7 +20,9 @@ import { SourcesMultiSelectDropdownComponent } from './components/sources-multi-
     CommonModule,
     RouterModule,
     NgMultiSelectDropDownModule.forRoot(),
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
+    LazyLoadImageModule,
+
   ],
   declarations: [AllNewsComponent, DisplayNewsComponent, TodayNewsComponent, FavoritesComponent, SpinnerComponent, SourcesMultiSelectDropdownComponent],
 
